52d4b253b5a3dfd52f38f2f857571f339250e68b
justice0317 <152950658+justice0317@users.noreply.github.com>
2024-11-02 15:16:27 +0800

    Update script.js

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ function uploadVideo() {
     loadingText.style.display = 'block';
     resultContainer.style.display = 'none';
 
-    fetch('http://54.150.30.200:5000/upload_video', {
+    fetch('https://098e-2001-b400-e2a6-b298-dca8-f638-2c44-43bc.ngrok-free.app/upload', {
         method: 'POST',
         body: formData
     })
@@ -69,4 +69,8 @@ function resetFileSelection() {
     commentCell.textContent = '';
 
     fileUploadLabel.classList.remove('has-video');
+}
+
+function goBack() {
+    window.location.href = 'https://previewer.adalo.com/9f754723-deab-48b2-837c-9cc329f37f6b?_gl=1%2A1k5c5wn%2A_gcl_au%2AOTU4ODkyOTMzLjE3MjkxNjUzMTY.%2A_ga%2ANTk3NzEyMDEwLjE3MjkxNjUzMjI.%2A_ga_SWT45DV35L%2AMTczMDUyNjcxNS4yMS4xLjE3MzA1MjY3MzIuNDMuMC4w&target=b3cb5rbulwpgvo5go895d19x8&params=%7B%7D';
 }
